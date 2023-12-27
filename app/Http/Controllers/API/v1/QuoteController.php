4a90a1d24b6ddf14f25a528632e9ddc521dd5ef9
jorge <jbousing@gmail.com>
2023-12-26 22:26:25 -0400

    test for Quotes API are passed

--- app/Http/Controllers/API/v1/QuoteController.php
+++ app/Http/Controllers/API/v1/QuoteController.php
@@ -4,40 +4,104 @@ namespace App\Http\Controllers\API\v1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\QuoteCollection;
+use App\Http\Resources\QuoteResource;
 use App\Models\Quote;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Http;
 
 class QuoteController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index(Request $request)
+    public function searchQuoteByText($text)
     {
-        $request->validate([
-            "user" => "required|numeric"
-        ]);
+        $result = Quote::where("text",$text)->get();
 
-        $data = $request->all();
+        if($result->count() > 0) {
+            return true;
+        }
 
-        $quotes = Quote::where("user_id",$data["user"])->paginate(10);
-
-        $user = User::find($data["user"]);
-
-        return response()->json([
-            "status" => 200,
-            "data" => new QuoteCollection($quotes),
-            "message" => "Get favorites quote of "
-        ]);
+        return false;
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function index(Request $request)
     {
-        //
+        $data = $request->all();
+
+        if(isset($data["qtx"]) && isset($data["user"])) {
+
+            //GET USER`S FAVORITES QUOTES AND QTX
+
+            $quotes = Quote::where("user_id",$data["user"])
+                            ->take($data["qtx"])
+                            ->get();
+
+            return response()->json([
+                "status" => 200,
+                "data" => $quotes,
+                "message" => "Get favorites quote of"
+            ]);
+
+        }else if(!isset($data["qtx"]) && isset($data["user"])) {
+
+            //GET ALL USER`S FAVORITES QUOTES WITH PAGINATION TO DONT BE SO HEAVY DATA
+
+            $quotes = Quote::where("user_id",$data["user"])->paginate(10);
+
+            return response()->json([
+                "status" => 200,
+                "data" => new QuoteCollection($quotes),
+                "message" => "Get favorites quote"
+            ]);
+
+        }else if(isset($data["qtx"]) && !isset($data["user"])) {
+
+            //GET RANDOM QUOTES FROM KANYE WEST API AND SPECIFIC QTX
+            $quotes = [];
+
+            for ($i = 0; $i < $data["qtx"]; $i++) {
+
+                $result = Http::get("https://api.kanye.rest")->body();
+                $item = json_decode($result);
+
+                $item->isFavorite = $this->searchQuoteByText($item->quote);
+
+                array_push($quotes,$item);
+
+            }
+
+            return response()->json([
+                "status" => 200,
+                "data" => $quotes,
+                "message" => "Get random quotes from API Kanye West"
+            ]);
+
+        }else {
+
+            //GET 5 RANDOM QUOTES
+
+            $quotes = [];
+
+            for ($i = 0; $i < 5; $i++) {
+
+                $result = Http::get("https://api.kanye.rest")->body();
+                $item = json_decode($result);
+
+                $item->isFavorite = $this->searchQuoteByText($item->quote);
+
+                array_push($quotes,$item);
+
+            }
+
+            return response()->json([
+                "status" => 200,
+                "data" => $quotes,
+                "message" => "Get ramdom quotes from API Kanye West"
+            ]);
+
+        }
+
+
+
     }
 
     /**
@@ -45,22 +109,24 @@ class QuoteController extends Controller
      */
     public function show(Quote $quote)
     {
-        //
+        return response()->json([
+            "status" => 200,
+            "data" => new QuoteResource($quote),
+            "message" => "Get Quote success"
+        ]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Quote $quote)
-    {
-        //
-    }
 
     /**
      * Remove the specified resource from storage.
      */
     public function destroy(Quote $quote)
     {
-        //
+        $quote->delete();
+
+        return response()->json([
+            "status" => 200,
+            "message" => "Delete favorite quote success"
+        ]);
     }
 }
--- tests/Feature/Http/Controllers/API/v1/QuoteControllerTest.php
+++ tests/Feature/Http/Controllers/API/v1/QuoteControllerTest.php
@@ -45,8 +45,112 @@ class QuoteControllerTest extends TestCase
             $quote->save();
         }
 
-        $response = $this->get("api/v1/quotes?user={$user->id}");
+        $response = $this->get("api/v1/quotes?user={$user->id}",[
+            "Content-type" => "application/json",
+            "Accept" => "application/json"
+        ]);
         $response->assertStatus(401);
     }
+
+    public function test_get_quotes_by_specif_quantity_and_user()
+    {
+        $user = User::factory()->create();
+        Sanctum::actingAs($user);
+
+        $quotes = Quote::factory(50)->create();
+
+        foreach ($quotes as $quote) {
+            $quote->user_id = $user->id;
+            $quote->save();
+        }
+
+        $qtx = 8;
+
+        $response = $this->get("api/v1/quotes?user={$user->id}&qtx={$qtx}");
+        $response->assertStatus(200);
+
+        $data = $response->json()["data"];
+
+        $this->assertTrue(count($data) === $qtx);
+    }
+
+    public function test_get_quotes_by_specif_quantity_and_without_user()
+    {
+        $user = User::factory()->create();
+        Sanctum::actingAs($user);
+
+        $quotes = Quote::factory(50)->create();
+
+        foreach ($quotes as $quote) {
+            $quote->user_id = $user->id;
+            $quote->save();
+        }
+
+        $qtx = 10;
+
+        $response = $this->get("api/v1/quotes?qtx={$qtx}");
+        $response->assertStatus(200);
+
+        $data = $response->json()["data"];
+
+        $this->assertTrue(count($data) === $qtx);
+    }
+
+    public function test_get_quotes_by_five_random()
+    {
+        $user = User::factory()->create();
+        Sanctum::actingAs($user);
+
+        $quotes = Quote::factory(50)->create();
+
+        foreach ($quotes as $quote) {
+            $quote->user_id = $user->id;
+            $quote->save();
+        }
+
+        $qtx = 5;
+
+        $response = $this->get("api/v1/quotes");
+        $response->assertStatus(200);
+
+        $data = $response->json()["data"];
+
+        $this->assertTrue(count($data) === $qtx);
+    }
+
+    public function test_show()
+    {
+        $user = User::factory()->create();
+        Sanctum::actingAs($user);
+
+        $quotes = Quote::factory(50)->create();
+
+        $response = $this->get("api/v1/quotes/{$quotes[0]->id}");
+        $response->assertStatus(200);
+
+        $data = $response->json()["data"];
+
+        $this->assertTrue($data["id"] === $quotes[0]->id);
+    }
+
+    public function test_destroy()
+    {
+        $user = User::factory()->create();
+        Sanctum::actingAs($user);
+
+        $quote = Quote::factory()->create();
+        $quote->user_id = $user->id;
+        $quote->save();
+
+        $response = $this->delete("api/v1/quotes/{$quote->id}");
+        $response->assertStatus(200);
+
+        $this->assertDatabaseMissing("quotes",[
+            "user_id" => $user->id,
+            "text" => $quote->text
+        ]);
+    }
+
+
 
 }
